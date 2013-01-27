9d8284029ed296d4cd784e788c140433e0f0a3ea
seph <leezhou88@hotmail.com>
2013-01-27 16:34:02 +0000

    Initial Commit

---
+++ RLScrumVoter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RLScrumVoter
+//
+//  Created by Ruoli Zhou on 27/01/2013.
+//  Copyright (c) 2013 LeeMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
